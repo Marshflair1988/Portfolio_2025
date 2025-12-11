d4c45fe5364fba3f22cd10c0df4a3301515f119f
Marsh Woolgar <104839692+Marshflair1988@users.noreply.github.com>
2025-12-11 15:32:56 +0100

    Update App.tsx
    Integrated Skills section into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
+import Skills from './components/Skills';
 function App() {
   return (
     <div className="min-h-screen">
