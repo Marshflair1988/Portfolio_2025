496402895901362d4b1f4a0d6747d8a4b1117681
Marsh Woolgar <104839692+Marshflair1988@users.noreply.github.com>
2025-12-11 15:27:19 +0100

    Updated app.tsx
    integrated about section into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
+import About from './components/About';
 function App() {
   return (
     <div className="min-h-screen">
